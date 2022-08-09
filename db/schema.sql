3bb92e8275e029b9184ba40a8bcbe1dc7d78de5f
cultstatue <saltedchurros@gmail.com>
2022-08-09 17:23:22 -0400

    added logic to create database and departments table using mysql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (name)
+VALUES
+('Markerting'),
+('Human Rescources'),
+('Finance'),
+('IT');
