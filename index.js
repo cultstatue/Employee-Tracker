1b935a44cf8b66b175d8ef275c1606b40fa001fa
cultstatue <saltedchurros@gmail.com>
2022-08-10 02:09:56 -0400

    added function to query for employee data

--- index.js
+++ index.js
@@ -43,3 +43,17 @@ getRoles = function() {
     .catch(console.log)
     .then( () => db.end());
 }
+
+getEmployees = function() {
+
+    const sql = 'SELECT employees.id, employees.first_name, employees.last_name, manager_id, roles.title, roles.salary, departments.name AS department FROM employees LEFT JOIN roles ON roles.id = employees.role_id LEFT JOIN departments ON departments.id = roles.dep_id;';
+
+    db.promise().query(sql)
+    .then( ([rows,fields]) => {
+
+        console.table('Employees', rows);
+
+    })
+    .catch(console.log)
+    .then( () => db.end());
+}
