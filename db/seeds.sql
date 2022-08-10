ec960b3a0874e18c39390cac18f471d5915887a8
cultstatue <saltedchurros@gmail.com>
2022-08-09 22:31:41 -0400

    added table for roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,13 @@ VALUES
 ('Human Rescources'),
 ('Finance'),
 ('IT');
+
+INSERT INTO roles (name, salary, dep_id)
+VALUES
+('Technical Specialist', 100, 4),
+('Sales Associate', 70, 1),
+('Accountant', 80, 3),
+('Talent Aquisition', 80, 3);
+SELECT roles.*, departments.name AS dep_name
+FROM roles
+LEFT JOIN departments ON roles.dep_id = departments.id;
