ca9df4fdb9140aec46ac30728fe8146331914956
cultstatue <saltedchurros@gmail.com>
2022-08-09 22:40:54 -0400

    added function to retrieve roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,7 +10,4 @@ VALUES
 ('Technical Specialist', 100, 4),
 ('Sales Associate', 70, 1),
 ('Accountant', 80, 3),
-('Talent Aquisition', 80, 3);
-SELECT roles.*, departments.name AS dep_name
-FROM roles
-LEFT JOIN departments ON roles.dep_id = departments.id;
+('Talent Aquisition', 80, 2);
--- index.js
+++ index.js
@@ -17,13 +17,29 @@ const db = mysql.createConnection({
 
 getDepartments = function() {
 
-    db.promise().query("SELECT * FROM departments")
+    const sql = "SELECT * FROM departments;";
+
+    db.promise().query(sql)
     .then( ([rows,fields]) => {
 
-        console.table(rows);
+        console.table('Departments', rows);
 
     })
     .catch(console.log)
     .then( () => db.end());
 
+}
+
+getRoles = function() {
+
+    const sql = 'SELECT roles.*, departments.name AS dep_name FROM roles LEFT JOIN departments ON roles.dep_id = departments.id;'
+
+    db.promise().query(sql)
+    .then( ([rows,fields]) => {
+
+        console.table('Roles', rows);
+
+    })
+    .catch(console.log)
+    .then( () => db.end());
 }
